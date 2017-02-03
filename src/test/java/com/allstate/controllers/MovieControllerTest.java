6d96bdc8064f4c75b41de32de5947123d618512b
Varun and Mike <VarunKumar2016@u.northwestern.edu>
2017-02-03 15:15:43 -0600

    Bollywood Service

---
+++ src/test/java/com/allstate/controllers/MovieControllerTest.java
@@ -0,0 +1,7 @@
+package com.allstate.controllers;
+
+/**
+ * Created by localadmin on 2/3/17.
+ */
+public class MovieControllerTest {
+}
---
+++ src/test/resources/sql/seed.sql
@@ -0,0 +1,7 @@
+
+use bollywoodt;
+
+TRUNCATE TABLE movies;
+
+INSERT INTO movies (title, rating, released, length, watched) VALUES
+  ('The Avengers', 'PG', '2015-03-15 00:00:00.000', 165, 1);
